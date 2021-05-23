e214f2adfaa990fe8f25e35d3a382967ff6dcbc6
Rounak-WHJr <81224582+Rounak-WHJr@users.noreply.github.com>
2021-05-23 14:03:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var tom,jerry;
+var tomImg1,tomImg2,tomImg3;
+var bg
+
+function preload() {
+    //load the images here
+    bg=loadImage("images/")
+}
+
+function setup(){
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
+
+}
+
+function draw() {
+
+    background(255);
+    //Write condition here to evalute if tom and jerry collide
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+
+
+}
